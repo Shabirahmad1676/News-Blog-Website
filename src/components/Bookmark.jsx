9a5a2e641b0a37e81cd55f61568971b82cf6ac82
Shabirahmad1676 <sa3835746@gmail.com>
2025-03-03 14:58:28 +0500

    Done Bookmarking

--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -1,27 +1,50 @@
-import React from 'react'
-import { CgClose } from 'react-icons/cg'
+import React from "react";
+import { CgClose } from "react-icons/cg";
 
-const Bookmark = ({isOpen,onClose}) => {
-  //  if(!isOpen){
-  //   return null
-  // }
+const Bookmark = ({
+  show,
+  bookmark,
+  onClose,
+  onSelectArticle,
+  onDeleteBookmark,
+}) => {
+  if(!show){
+    return null
+  }
 
- 
   return (
-    <div className="absolute top-20 rounded-2xl  left-[26%]  gap-3 text-white bg-emerald-400 flex-col shadow-2xl flex items-start max-w-[580px]  justify-center p-4 z-50">
+    <div className="absolute top-20 rounded-2xl  left-[26%] w-[50rem] gap-3 text-white bg-emerald-400 flex-col shadow-2xl flex items-start max-w-[580px]  justify-center p-4 z-50">
       <h1>Your Favourites</h1>
-      <button onClick={onClose} className='absolute right-3 top-1 cursor-pointer font-extrabold bg-gray-800 rounded-full p-1 text-2xl'><CgClose/></button>
-      <div className="relative w-[100%] max-w-[45rem]  h-auto shadow-2xl text-white flex gap-6 flex-col  items-start  bg-[#0F1113] p-6 rounded-2xl z-50">
+      <button
+        onClick={onClose}
+        className="absolute right-3 top-1 cursor-pointer font-extrabold bg-gray-800 rounded-full p-1 text-2xl"
+      >
+        <CgClose />
+      </button>
+      <div className="relative max-w-[45rem] w-[100%] h-auto shadow-2xl text-white flex gap-6 flex-col  items-start  bg-[#0F1113] p-6 rounded-2xl z-50">
         {/* {article && ( */}
-          <div className='flex gap-4 items-center border-b'>
-          <img src=".public/react.jpg" alt="" className='h-14 w-20 object-cover' />
-          <h1 className='font-bold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, deserunt.</h1>
-          <button className=' cursor-pointer font-extrabold bg-gray-800 rounded-full p-1 text-2xl'><CgClose/></button>  
-        </div>
+       {bookmark.map((article,index)=>(
+         <div className="flex gap-4 items-center border-b" key={index} onClick={()=>onSelectArticle(article)}>
+         <img
+           src={article.image}
+           alt=""
+           className="h-14 w-20 object-cover"
+         />
+         <h1 className="font-bold">
+           {article.title}
+         </h1>
+         <button onClick={(e)=>{
+          e.stopPropagation()
+          onDeleteBookmark(article)
+         }} className=" cursor-pointer font-extrabold bg-gray-800 rounded-full p-1 text-2xl">
+           <CgClose />
+         </button>
+       </div>
+       ))}
         {/* )} */}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Bookmark
+export default Bookmark;
